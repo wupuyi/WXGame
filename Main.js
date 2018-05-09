6fa0634f58b7026af91eade8b171fdfdffb805cd
Puyi Wu <wupuyi@foxmail.com>
2018-05-09 19:23:24 +0800

    游戏资源加载

--- Main.js
+++ Main.js
@@ -1,6 +1,15 @@
+import { ResourceLoader } from "./js/base/ResourceLoader.js";
+
 // 初始化整个游戏的精灵，作为游戏开始的入口
 export class Main {
   constructor () {
-    console.log('我执行了')
+    this.canvas = document.getElementById('game_canvas')
+    this.ctx = this.canvas.getContext('2d')
+    const loader = ResourceLoader.create()
+    loader.onLoaded(map => this.onResourceFirstLoaded(map))
+  }
+
+  onResourceFirstLoaded (map) {
+    console.log(map)
   }
 }
