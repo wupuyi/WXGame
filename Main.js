d1dd754fb50e62b193f40cbc3bef5912ffa1df01
Puyi Wu <wupuyi@foxmail.com>
2018-05-21 17:22:23 +0800

    实现小鸟,并且是小鸟能够掉下来

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ import { Director } from "./js/Director.js";
 import { BackGround } from "./js/runtime/BackGround.js";
 import { DataStore } from "./js/base/DataStore.js";
 import { Land } from "./js/runtime/Land.js";
+import { Birds } from "./js/player/Birds.js";
 
 // 初始化整个游戏的精灵，作为游戏开始的入口
 export class Main {
@@ -28,7 +29,8 @@ export class Main {
     this.dataStore
         .put('pencils', [])
         .put('background', BackGround)
-        .put('land', Land);
+        .put('land', Land)
+        .put('birds', Birds);
     // let background = new BackGround(this.ctx, map.get('background'));
     // background.draw();
     // 创建铅笔要在游戏逻辑运行之前
