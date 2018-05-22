638b19c453504d9cacef0b9c9ceccf5ec7b8d0e6
Puyi Wu <wupuyi@foxmail.com>
2018-05-22 13:36:18 +0800

    测试HTTP请求

--- test/request_server.js
+++ test/request_server.js
@@ -1,2 +1,19 @@
 // 用来模拟服务器，接受前端小游戏的HTTP网络请求
 // 通过node来模拟
+(function () {
+  'use strict';
+  const http = require('http');
+  // 创建http服务器
+  // http短链接与websocket长连接区分
+  http.createServer(function (request, response) {
+    let body = '';
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
+    // 请求结束
+    request.on('end', function () {
+      response.end('这是服务器返回的数据')
+      console.log(body);
+    })
+  }).listen(8181);
+})();
