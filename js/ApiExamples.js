7e0c52a5dc2f5db801b22f0186cedda9017221ed
Puyi Wu <wupuyi@foxmail.com>
2018-05-22 13:52:39 +0800

    测试WebSocket

--- js/ApiExamples.js
+++ js/ApiExamples.js
@@ -46,4 +46,27 @@ export class ApiExamples {
       }
     })
   }
+
+  socketExample () {
+    wx.connectSocket({
+      // WebSocket协议为ws不是http
+      url: 'ws://127.0.0.1:8282',
+      success: function () {
+        console.log('客户端链接成功')
+      }
+    });
+
+    // 注意发送数据，必须在wx.onSocketOpen(callback)方法中进行
+
+    wx.onSocketOpen(function () {
+      wx.sendSocketMessage({
+        data: '这个是来客户端的实时消息'
+      });
+
+      wx.onSocketMessage(function (message) {
+        console.log(message)
+      });
+    });
+
+  }
 }
